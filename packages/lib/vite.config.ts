d612961383448b64b4167bee1bdc27c600682b4f
xujianxiang <584972029@qq.com>
2023-06-24 08:49:16 +0800

    feat: add core & lib

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+
+
+const resolvePath = (str: string) => path.resolve(__dirname, str);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolvePath("src/index.ts"),
+      name: "lib",
+      fileName: format => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "react-dom",
+        },
+      },
+    },
+  }
 })
