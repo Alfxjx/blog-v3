2c0ad72b2f3f0cf790be4598658bb224b8c44c0f
xujianxiang <584972029@qq.com>
2023-08-04 17:36:16 +0800

    feat: add buy me a coffee button

---
+++ packages/app/src/components/buttons/buy-me-a-coffee.tsx
@@ -0,0 +1,9 @@
+export const BuyMeCoffee = () => (
+  <a href="https://www.buymeacoffee.com/alfxjx" target="_blank">
+    <img
+      src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+      alt="Buy Me A Coffee"
+      style={{ height: '60px', width: '217px' }}
+    />
+  </a>
+);
