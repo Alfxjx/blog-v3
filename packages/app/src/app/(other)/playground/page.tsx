42a7fe9d1fd0748ed272b7515298f4a2f87711de
xujianxiang <584972029@qq.com>
2023-12-25 15:05:18 +0800

    feat: add rough-notation

--- packages/app/src/app/(other)/playground/page.tsx
+++ packages/app/src/app/(other)/playground/page.tsx
@@ -1,9 +1,19 @@
-import WordTree from '@/components/word-tree';
+'use client';
+import React, { useEffect } from 'react';
+import { annotate } from 'rough-notation';
 
 export default async function Page() {
+  useEffect(() => {
+    const e = document.querySelector<HTMLElement>('#myElement');
+    if (e) {
+      const annotation = annotate(e, { type: 'highlight', color: '#f9d423' });
+      annotation.show();
+    }
+  }, []);
+
   return (
     <div className="w-full h-96 mx-auto">
-      <WordTree />
+      <span id="myElement">hello notation</span>
     </div>
   );
 }
--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -3,15 +3,28 @@ import { Button } from '@/components/buttons/slide-button';
 import { caveat } from '@/components/layouts/fonts';
 import { useRouter } from 'next/navigation';
 import WordTree from '@/components/word-tree';
+import { RoughNotation } from 'react-rough-notation';
 
 export default function Home() {
   const router = useRouter();
   return (
     <main className="flex flex-col items-center relative">
       <h1
-        className={`text-[4rem] mt-24 flex justify-center ${caveat.className}`}
+        className={`text-[2rem] md:text-[4rem] leading-normal mt-24 flex flex-col items-center justify-center ${caveat.className}`}
       >
-        Alfr3d's Blog
+        <p>Alfr3d's Blog:</p>
+        <p>
+          <span className="mr-2">My own</span>
+          <RoughNotation
+            type="circle"
+            show={true}
+            color="green"
+            strokeWidth={2}
+            animationDelay={250}
+          >
+            digital garden.
+          </RoughNotation>
+        </p>
       </h1>
       <div className="mt-24 z-10">
         <Button label="slide" onClick={() => router.push('/blogs')}></Button>
